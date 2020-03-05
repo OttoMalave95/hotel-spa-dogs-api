c296669a2844af4701e4d2be54b34bfa383fe9b3
Otto Malavé <ottoantonio20@gmail.com>
2020-03-05 19:58:27 -0400

    Modificadas las funciones para editar un perro y hotel

--- server/database/update.js
+++ server/database/update.js
@@ -1,33 +1,55 @@
-var mongoose = require("./config/mongoose-module");
-var Proyecto = require("./models/proyecto");
+const Hotel = require('./models/hotel');
+const Perro = require('./models/perro');
 
 module.exports = {
+  async editarPerro(perro) {
+    if(perro.id)var id = perro.id;
+    if(perro._id) var id = perro._id;
 
-  async editarProyecto(proyecto) {
-    if(proyecto.id)var id = proyecto.id;
-    if(proyecto._id) var id = proyecto._id;
+    return Perro.findOne({_id: id}).then(data => {
 
-    return Proyecto.findOne({_id: id}).then(data => {
-      
-      if (proyecto["titulo"]) data["titulo"] = proyecto["titulo"];
-      if (proyecto["objetivo"]) data["objetivo"] = proyecto["objetivo"];
-      if (proyecto["tipo_proyecto"]) data["tipo_proyecto"] = proyecto["tipo_proyecto"];
-      if (proyecto["presupuesto"]) data["presupuesto"] = proyecto["presupuesto"];
-      if (proyecto["materiales"]) data["materiales"] = proyecto["materiales"];
-      if (proyecto["duracion"]) data["duracion"] = proyecto["duracion"];
-      if (proyecto["nombre"]) data["responsable"]["nombre"] = proyecto["nombre"];
-      if (proyecto["apellido"]) data["responsable"]["apellido"] = proyecto["apellido"];
-      if (proyecto["cedula"]) data["responsable"]["cedula"] = proyecto["cedula"];
-      if (proyecto["correo_electronico"]) data["responsable"]["correo_electronico"] = proyecto["correo_electronico"];
-      
+      if (perro["cedula"]) data["cedula"] = perro["cedula"];
+      if (perro["nombre"]) data["nombre"] = perro["nombre"];
+      if (perro["especie"]) data["especie"] = perro["especie"];
+      if (perro["sexo"]) data["sexo"] = perro["sexo"];
+      if (perro["raza"]) data["raza"] = perro["raza"];
+      if (perro["color"]) data["color"] = perro["color"];
+      if (perro["registros"]) data["registros"] = perro["registros"];
+
       return data.save(data).then((data)=>{
         return {
-          Proyecto: data,
-          message: "Proyecto modificado con exito"
+          Perro: data,
+          message: "Perro modificado con exito"
         };
       }).catch(err => {
         return {
-          message: "Error al modificar los datos del proyecto"
+          message: "Error al modificar los datos del perro"
+        }
+      });
+    });
+  },
+
+  async editarHotel(hotel) {
+    if(hotel.id)var id = hotel.id;
+    if(hotel._id) var id = hotel._id;
+
+    return Hotel.findOne({_id: id}).then(data => {
+
+      if (hotel["nombre"]) data["nombre"] = hotel["nombre"];
+      if (hotel["rif"]) data["rif"] = hotel["rif"];
+      if (hotel["habitaciones"]) data["habitaciones"] = hotel["habitaciones"];
+      if (hotel["spa"]["baño"]) data["spa"]["baño"] = hotel["spa"]["baño"];
+      if (hotel["spa"]["peluqueria"]) data["spa"]["peluqueria"] = hotel["spa"]["peluqueria"];
+      if (hotel["spa"]["manicura"]) data["spa"]["manicura"] = hotel["spa"]["manicura"];
+
+      return data.save(data).then((data)=>{
+        return {
+          Hotel: data,
+          message: "Hotel modificado con exito"
+        };
+      }).catch(err => {
+        return {
+          message: "Error al modificar los datos del hotel"
         }
       });
     });
